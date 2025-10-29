3924ca4bdd75b94488bd88ff9e3f87788857b161
williamgore <williamgore241@gmail.com>
2025-10-29 14:43:57 -0500

    Added a period

--- script.js
+++ script.js
@@ -40,6 +40,6 @@ function removeTitle(s, title) {
 
 ////////////////////////////////////////////////////////////
 
-let titles = ["a gamer", "a hard worker", "a dedicated learner", "the right choice"];
+let titles = ["a gamer", "a hard worker", "a dedicated learner", "the right choice."];
 updateTitles(titles)
 
