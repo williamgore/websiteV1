5dcf3b714ef2b559a34930449de963e39cb7f6af
williamgore <williamgore241@gmail.com>
2025-10-29 14:39:21 -0500

    Added awesome JS

---
+++ script.js
@@ -0,0 +1,45 @@
+function updateElement(newString, ID) {
+    document.getElementById(ID).innerHTML = newString;
+    console.log("Executed")
+}
+
+function updateTitles(titles) {
+    s = "I am "
+    waitTime = 0;
+    waitToDelete = 3000
+
+    for(let i = 0; i<titles.length; i++) {
+        toAdd = titles[i]
+        charTime = ((toAdd.length) * 100) * 2
+
+
+        setTimeout(addNewTitle, waitTime, s, toAdd)
+        
+        if(i < titles.length - 1) {
+            setTimeout(removeTitle, (waitTime + waitToDelete), (s + toAdd), toAdd)
+        }
+
+        waitTime = waitTime + charTime + waitToDelete
+    }
+
+}
+
+function addNewTitle(s, title) {
+    for (let i = 0; i < title.length; i++) {
+        s = s + title.charAt(i);
+        setTimeout(updateElement,(100*i), s, "me")
+    }
+}
+
+function removeTitle(s, title) {
+    for (let i = 0; i < title.length; i++) {
+        s = s.substring(0, s.length - 1);
+        setTimeout(updateElement,(100*i), s, "me")
+    }
+}
+
+////////////////////////////////////////////////////////////
+
+let titles = ["a student", "a gamer", "bruh im dumb"];
+updateTitles(titles)
+
